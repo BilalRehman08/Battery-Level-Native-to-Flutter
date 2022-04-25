173a3c55894e1d59cd9d29ff30e8098e01739cfe
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-25 13:52:13 +0500

    add imports in mainactivity.java

--- android/app/src/main/java/com/example/batterylevel/MainActivity.java
+++ android/app/src/main/java/com/example/batterylevel/MainActivity.java
@@ -2,6 +2,14 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+
 
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "samples.flutter.dev/battery";
